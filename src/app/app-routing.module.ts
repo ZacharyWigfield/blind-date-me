9feb98e646dbb7cd9869ba9c9c49b76e3d181853
Zachary Wigfield <zewigfield@gmail.com>
2020-12-27 11:08:33 -0500

    created a user module and user routing module. Enabled lazy loading for the user routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent }
+  { path: '', component: LandingPageComponent },
+  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
 ];
 
 @NgModule({
