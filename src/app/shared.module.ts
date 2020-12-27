afa9bf06f99d61665ae8fd818b7fbe325bd02813
Zachary Wigfield <zewigfield@gmail.com>
2020-12-27 00:05:11 -0500

    created navigation for desktop and mobile with content beneath

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -3,19 +3,27 @@ import { CommonModule } from '@angular/common';
 
 // material imports
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const components = []
 
-const modules = 
-[CommonModule,
-MatButtonModule
-]
+const modules =
+  [
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule
+  ]
 
 @NgModule({
   declarations: [...components],
   imports: [...modules],
   exports: [
-    MatButtonModule,
     ...components,
     ...modules
   ]
