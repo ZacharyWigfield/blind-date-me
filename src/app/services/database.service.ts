f5352d0e9bb76e1edf2529e8fe6d7df99183822a
Zachary Wigfield <zewigfield@gmail.com>
2021-02-11 01:10:25 -0500

    added function to retrieve user answers

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -11,15 +11,30 @@ export class DatabaseService {
 
   constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) { }
 
-  /**
-     * Creates a new answer array for the current user
-     */
+  // Creates a new answer array for the current user //
   async createAnswerArray(data: string[]) {
     const user = await this.afAuth.currentUser;
     return this.db.collection('Answers').add({
       answers: data,
       uid: user.uid,
     });
+  }
+
+  // Retrieves answer array owned by current user //
+  getUserBoards() {
+    return this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.db
+            .collection('Answers', ref =>
+              ref.where('uid', '==', user.uid).orderBy('priority')
+            )
+            .valueChanges({ idField: 'id' });
+        } else {
+          return [];
+        }
+      })
+    );
   }
 
 
